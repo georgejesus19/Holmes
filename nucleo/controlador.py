3c569175a0b52912b7ab46759d6195889c29a991
George Hebo <georgehebo18@gmail.com>
2026-04-10 19:03:41 +0100

    Criação do diretório que armazena funções do modo de análise manual do Holmes

---
+++ nucleo/controlador.py
@@ -0,0 +1,128 @@
+import os
+from modulos import interface
+from modulos import logs
+
+
+# =========================
+# ANÁLISE PRINCIPAL
+# =========================
+
+def analisar_processo():
+    pass
+
+def analisar_programa_chave_registo():
+    pass
+
+def analisar_tarefa_agendada():
+    pass
+
+def analisar_servico():
+    pass
+
+def analisar_conexao_rede():
+    pass
+
+
+# =========================
+# PROCESSOS DB
+# =========================
+
+def mostrar_processos_db():
+    logs.consultar_processos("processos")
+    input("Pressione enter para voltar ao menu do modo manual...")
+    os.system("cls")
+
+def mostrar_processos_suspeitos_db():
+    pass
+
+
+# =========================
+# HKCU
+# =========================
+
+def mostrar_programas_HKCU_db():
+    pass
+
+def mostrar_programas_suspeitos_HKCU_db():
+    pass
+
+
+# =========================
+# HKLM
+# =========================
+
+def mostrar_programas_HKLM_db():
+    pass
+
+def mostrar_programas_suspeitos_HKLM_db():
+    pass
+
+
+# =========================
+# TAREFAS AGENDADAS
+# =========================
+
+def mostrar_tarefas_agendadas_db():
+    pass
+
+def mostrar_tarefas_agendadas_suspeitas_db():
+    pass
+
+
+# =========================
+# SERVIÇOS
+# =========================
+
+def mostrar_servicos_db():
+    pass
+
+def mostrar_servicos_suspeitos_db():
+    pass
+
+
+# =========================
+# CONEXÕES DE REDE
+# =========================
+
+def mostrar_conexoes_db():
+    pass
+
+def mostrar_conexoes_suspeitas_db():
+    pass
+def modo_manual():
+    opcoes = {
+        1: analisar_processo,
+        2: analisar_programa_chave_registo,
+        3: analisar_tarefa_agendada,
+        4: analisar_servico,
+        5: analisar_conexao_rede,
+        6: mostrar_processos_db,
+        7: mostrar_processos_suspeitos_db,
+        8: mostrar_programas_HKCU_db,
+        9: mostrar_programas_suspeitos_HKCU_db,
+        10: mostrar_programas_HKLM_db,
+        11: mostrar_programas_suspeitos_HKLM_db,
+        12: mostrar_tarefas_agendadas_db,
+        13: mostrar_tarefas_agendadas_suspeitas_db,
+        14: mostrar_servicos_db,
+        15: mostrar_servicos_suspeitos_db,
+        16: mostrar_conexoes_db,
+        17: mostrar_conexoes_suspeitas_db,
+    }
+    os.system("cls")
+    while True:
+        opc = interface.menu_modo_manual()
+        os.system("cls")
+        funcao = opcoes.get(opc)
+
+        if (opc == 18):
+            break
+
+        if funcao:
+            funcao()
+        else:
+            print("Opção inválida")
+
+
+
+
