47b0084ca393dc14128fbf591de4596ec3044abf
George Hebo <georgehebo18@gmail.com>
2026-04-18 13:31:56 +0100

    Implementação de funcionalidade para análise individual de um único binário (função analisar_processo())

--- nucleo/controlador.py
+++ nucleo/controlador.py
@@ -1,15 +1,68 @@
 import os
+import psutil
+
 from modulos import interface
 from modulos import logs
+from uteis import normalizar_caminho
+from uteis import obter_hash
+from uteis import verificar_assinatura_digital
+
 
 mensagem = "Pressione enter para voltar ao menu do modo manual..."
+
+# =========================
+# FUNÇÕES COMUNS.
+# =========================
+
+def selecionar_valor(lista):
+    i = 0
+    for i, binario in enumerate(lista):
+        print(f"{i + 1} -> Nome: {binario['nome']}")
+    total = i
+
+    while True:
+        try:
+            opc = int(input(f"Selecione um valor entre 1 e {total + 1}: "))
+            if ((opc > total) or (opc <= 0)):
+                print("Selecione uma opção válida!")
+            else:
+                break
+        except (ValueError):
+            print("Selecione uma opção válida!")
+    return lista[opc - 1]
 
 # =========================
 # ANÁLISE PRINCIPAL
 # =========================
 
 def analisar_processo():
-    pass
+    os.system("cls")
+    temporario = dict() # Irá armazenar de forma temporário dados relativos a um processo.
+    processos = list() # Irá armazenar os respectivos dados vindos do dicionário temporário.
+    print("Processos disponíveis para análise: \n")
+    for process in psutil.process_iter(['name', 'exe']):
+        try:
+            caminho = process.exe()
+        except (psutil.AccessDenied, psutil.NoSuchProcess):
+            caminho = "Acesso negado ou processo terminado"
+        temporario['nome'] = process.name()
+        temporario['caminho'] = normalizar_caminho.normalizar(caminho)
+        if (temporario['nome'].endswith(".exe")):
+            processos.append(temporario.copy()) # porque que atribuo uma cópia ?
+    item = selecionar_valor(processos)
+
+    os.system("cls")
+    assinatura = "Válida" \
+    if verificar_assinatura_digital.verificar_assinatura(item['caminho']) == "Signature verified." else \
+    verificar_assinatura_digital.verificar_assinatura(item['caminho'])
+
+    print("Dados do binário:")
+    print("--------------------")
+    print(f"Nome: {item['nome']}")
+    print(f"Caminho: {item['caminho']}")
+    print(f"Hash do executável: {obter_hash.obter_hash(item['caminho'])}")
+    print(f"Estado da assinatura digital: {assinatura}")
+    print("--------------------")
 
 def analisar_programa_chave_registo():
     pass
@@ -23,6 +76,12 @@ def analisar_servico():
 def analisar_conexao_rede():
     pass
 
+# =========================
+# CONSULTA NA API VIRUSTOTAL
+# =========================
+
+def consultar_API():
+    pass
 
 # =========================
 # PROCESSOS DB
@@ -117,29 +176,33 @@ def modo_manual():
         3: analisar_tarefa_agendada,
         4: analisar_servico,
         5: analisar_conexao_rede,
-        6: mostrar_processos_db,
-        7: mostrar_processos_suspeitos_db,
-        8: mostrar_programas_HKCU_db,
-        9: mostrar_programas_suspeitos_HKCU_db,
-        10: mostrar_programas_HKLM_db,
-        11: mostrar_programas_suspeitos_HKLM_db,
-        12: mostrar_tarefas_agendadas_db,
-        13: mostrar_tarefas_agendadas_suspeitas_db,
-        14: mostrar_servicos_db,
-        15: mostrar_servicos_suspeitos_db,
-        16: mostrar_conexoes_db,
-        17: mostrar_conexoes_suspeitas_db,
+        6: consultar_API,
+        7: mostrar_processos_db,
+        8: mostrar_processos_suspeitos_db,
+        9: mostrar_programas_HKCU_db,
+        10: mostrar_programas_suspeitos_HKCU_db,
+        11: mostrar_programas_HKLM_db,
+        12: mostrar_programas_suspeitos_HKLM_db,
+        13: mostrar_tarefas_agendadas_db,
+        14: mostrar_tarefas_agendadas_suspeitas_db,
+        15: mostrar_servicos_db,
+        16: mostrar_servicos_suspeitos_db,
+        17: mostrar_conexoes_db,
+        18: mostrar_conexoes_suspeitas_db,
     }
+
     os.system("cls")
     while True:
         opc = interface.menu_modo_manual()
         os.system("cls")
         funcao = opcoes.get(opc)
 
-        if (opc == 18):
+        if (opc == 19):
             break
 
         if funcao:
             funcao()
+            input("Pressione enter para voltar...")
+            os.system("cls")
         else:
             print("Opção inválida")
