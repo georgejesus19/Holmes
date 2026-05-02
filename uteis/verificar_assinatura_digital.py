f4aa328a5e85b02f99879c854a8d9b5ee4800e7b
George Hebo <georgehebo18@gmail.com>
2026-05-02 16:59:21 +0100

    Padronização de mensagens de erro, para utilização no modelo de score

--- uteis/verificar_assinatura_digital.py
+++ uteis/verificar_assinatura_digital.py
@@ -11,19 +11,22 @@ def verificar_assinatura(caminho):
             comando = [
                 "powershell",
                 "-Command",
-                f"(Get-AuthenticodeSignature '{caminho}').StatusMessage"
+                f"(Get-AuthenticodeSignature '{caminho}').Status"
             ]
             resultado = subprocess.run(comando, capture_output=True, text=True)
             status_message = resultado.stdout.strip()
+
+            if status_message not in ['Valid', 'NotSigned', 'HashMismatch', 'NotTrusted']:
+                return "UnknownError"
+
             assinatura_cache[caminho] = status_message
             if resultado.returncode != 0 or resultado.stderr.strip():
                 # O PowerShell teve erro
                 #return f"{resultado.stderr.strip()}"
-                return "Erro do powershell"
+                return "UnknownError"
             return status_message
 
         except (FileNotFoundError, OSError, subprocess.CalledProcessError, PermissionError, IOError) as erro:
-            assinatura_cache[caminho] = f"Erro ao verificar assinatura {erro}"
-            return assinatura_cache[caminho]
+            return "UnknownError"
     else:
-        return "Não foi possível verificar assinatura digital (caminho inexistente)"
+        return "UnknownError"
