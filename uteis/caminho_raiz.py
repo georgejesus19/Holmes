255bfd4b0e1cbdbf75d02d80419c9c11129ca31f
George <georgehebo18@gmail.com>
2026-05-17 22:18:35 +0100

    Remoção de comentário desnecessário

--- uteis/caminho_raiz.py
+++ uteis/caminho_raiz.py
@@ -2,6 +2,5 @@ from pathlib import Path
 
 def verificar_caminho_raiz(caminho):
     p = Path(caminho)
-    # parent como string e normalize para barras
     parent_str = str(p.parent).replace("/", "\\")
     return (p.suffix.lower() == ".exe" and parent_str == p.anchor)
