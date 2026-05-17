5e68e062620469e4c0dc54105739a12306a64cd7
George <georgehebo18@gmail.com>
2026-05-17 22:19:38 +0100

    Remoção de verificação redundante de existência de caminhos

--- uteis/obter_hash.py
+++ uteis/obter_hash.py
@@ -2,14 +2,11 @@ import os
 import hashlib
 
 def obter_hash(caminho, tipo_hash="sha256"):
-    if (os.path.exists(caminho)):
-        hash_func = hashlib.new(tipo_hash)
-        try:
-            with open(caminho, "rb") as ficheiro:
-                for p in iter(lambda: ficheiro.read(4096), b""):
-                    hash_func.update(p)
-            return hash_func.hexdigest()
-        except (FileNotFoundError, IOError, NameError) as erro:
-            return f"Erro durante a obtenção do hash"
-    else:
-        return "Erro durante a obtenção do hash (caminho inexistente)"
+    hash_func = hashlib.new(tipo_hash)
+    try:
+        with open(caminho, "rb") as ficheiro:
+            for p in iter(lambda: ficheiro.read(4096), b""):
+                hash_func.update(p)
+        return hash_func.hexdigest()
+    except Exception as erro:
+        return f"Erro durante a obtenção do hash"
