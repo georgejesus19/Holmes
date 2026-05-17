24a8a933c699f46c3c4e57de6854e775ca6789e2
George <georgehebo18@gmail.com>
2026-05-17 22:22:56 +0100

    Remoção de verificação redundante: Existência do caminho do binário

--- uteis/verificar_assinatura_digital.py
+++ uteis/verificar_assinatura_digital.py
@@ -1,32 +1,28 @@
-import os
 import subprocess
 
 assinatura_cache = {}
 
 def verificar_assinatura(caminho):
-    if(os.path.exists(caminho)):
-        if caminho in assinatura_cache:
-            return assinatura_cache[caminho]
-        try:
-            comando = [
-                "powershell",
-                "-Command",
-                f"(Get-AuthenticodeSignature '{caminho}').Status"
-            ]
-            resultado = subprocess.run(comando, capture_output=True, text=True)
-            status_message = resultado.stdout.strip()
+    if caminho in assinatura_cache:
+        return assinatura_cache[caminho]
+    try:
+        comando = [
+            "powershell",
+            "-Command",
+            f"(Get-AuthenticodeSignature '{caminho}').Status"
+        ]
+        resultado = subprocess.run(comando, capture_output=True, text=True)
+        status_message = resultado.stdout.strip()
 
-            if status_message not in ['Valid', 'NotSigned', 'HashMismatch', 'NotTrusted']:
-                return "UnknownError"
-
-            assinatura_cache[caminho] = status_message
-            if resultado.returncode != 0 or resultado.stderr.strip():
-                # O PowerShell teve erro
-                #return f"{resultado.stderr.strip()}"
-                return "UnknownError"
-            return status_message
-
-        except (FileNotFoundError, OSError, subprocess.CalledProcessError, PermissionError, IOError) as erro:
+        if status_message not in ['Valid', 'NotSigned', 'HashMismatch', 'NotTrusted']:
             return "UnknownError"
-    else:
+
+        assinatura_cache[caminho] = status_message
+        if resultado.returncode != 0 or resultado.stderr.strip():
+            # O PowerShell teve erro
+            #return f"{resultado.stderr.strip()}"
+            return "UnknownError"
+        return status_message
+
+    except Exception as erro:
         return "UnknownError"
