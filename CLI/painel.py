900a975bfbb49ffc47f9f9593a2190e22cea725c
George <georgehebo18@gmail.com>
2026-06-05 17:35:41 +0100

    Adição de cores ao painel

--- CLI/painel.py
+++ CLI/painel.py
@@ -1,104 +1,109 @@
 from rich.panel import Panel
 from rich import print
+from CLI.cores import CORES
 
 def painel_de_processo(pid, ppid, nome, caminho, utilizador, hash, estado_da_assinatura, pontuacao, nivel, motivos):
     painel = Panel(f"""
-    Identificação: \n
+    [bold blue]Identificação:[/bold blue]\n
     • PID: {pid}
     • PPID: {ppid}
     • Nome: {nome}
     • Utilizador do processo: {utilizador}\n
-    Executável: \n
+    [yellow]Executável:[/yellow] \n
     • Caminho: {caminho}
     • Hash: {hash}
     • Estado da assinatura digital: {estado_da_assinatura}\n
-    Avaliação: \n
+    [bold green]Avaliação:[/bold green] \n
     • Pontuação de risco: {pontuacao}
     • Nível de risco: {nivel}
     • Motivos: {motivos}
-                            """,
-                   title="Detalhes do processo", width=85)
+    """,
+
+    title="Detalhes do processo", width=85)
     print(painel)
 
 def painel_chaves_registo(nome, hk, caminho, hash, estado_da_assinatura, pontuacao, nivel, motivos):
     painel = Panel(f"""
-    Identificação: \n
+    [bold blue]Identificação:[/bold blue]\n
     • Nome: {nome}
-    • HIVE_KEY: {hk}
-    Executável: \n
+    • HIVE_KEY: {hk}\n
+    [yellow]Executável:[/yellow]\n
     • Caminho: {caminho}
     • Hash: {hash}
     • Estado da assinatura digital: {estado_da_assinatura}\n
-    Avaliação: \n
+    [bold green]Avaliação:[/bold green]\n
     • Pontuação de risco: {pontuacao}
     • Nível de risco: {nivel}
     • Motivos: {motivos}
-    
-                        """,
-                   title="Detalhes do programa", width=85)
+    """,
+
+    title="Detalhes do programa", width=85)
     print(painel)
 
 def painel_tarefas_agendadas(nome, ultima_execucao, proxima_execucao, caminho, utilizador,
                              hash, estado_da_assinatura, pontuacao, risco, motivos):
 
     painel = Panel(f"""
-    Identificação: \n
+    [bold blue]Identificação:[/bold blue]\n
     • Nome: {nome}
     • Utilizador da tarefa: {utilizador} \n
-    Executável: \n
+    [yellow]Executável:[/yellow]\n
     • Caminho: {caminho}
     • Hash: {hash}
     • Estado da assinatura digital: {estado_da_assinatura} 
     • Última execução: {ultima_execucao}
     • Próxima execução: {proxima_execucao}\n
-    Avaliação: \n
+    [bold green]Avaliação:[/bold green]\n
     • Pontuação de risco: {pontuacao}
     • Nível de risco: {risco}
     • Motivos: {motivos}
-                """,
-                title="Detalhes da tarefa agendada", width=85)
+    """,
+
+    title="Detalhes da tarefa agendada", width=85)
     print(painel)
 
 def painel_servicos(nome, nome_exibido, caminho, hash, estado_da_assinatura, pontuacao, risco, motivos):
     painel = Panel(f"""
-    Indentificação: \n
+    [bold blue]Indentificação:[/bold blue]\n
     • Nome do serviço: {nome}
     • Nome exibido: {nome_exibido} \n
-    Executável: \n
+    [yellow]Executável:[/yellow]\n
     • Caminho: {caminho}
     • Hash: {hash}
     • Estado da assinatura digital: {estado_da_assinatura} \n
-    Avaliação: \n
+    [bold green]Avaliação:[/bold green]\n
     • Pontuação de risco: {pontuacao} 
     • Nível de risco: {risco}
     • Motivos: {motivos}
-                        """,
-                   title="Detalhes do serviço", width=85)
+    """,
+
+    title="Detalhes do serviço", width=85)
     print(painel)
 
 def painel_conexoes_rede(ip_local, porta_local, endereco_remota, dominio, porta_remota,
                          estado, pid, ppid, nome, caminho, hash, estado_da_assinatura, pontuacao, risco, motivos):
     painel = Panel(f"""
-    Indentificação: \n
+    [bold blue]Indentificação:[/bold blue]\n
     • PID: {pid}
     • PPID: {ppid}
     • Nome: {nome} \n
-    Executável: \n
+    [yellow]Executável:[/yellow]\n
     • Caminho: {caminho}
     • Hash: {hash}
     • Estado da assinatura digital: {estado_da_assinatura} \n
-    Conexão: \n
+    [bold magenta]Conexão:[/bold magenta]\n
     • Ip local: {ip_local}
     • Porta local: {porta_local}
     • Endereço remoto: {endereco_remota}
     • Porta remota: {porta_remota}
     • Estado da ligação: {estado} \n
-    Destino: \n
-    •Domínio: {dominio} \n
-    Avaliação: \n
+    [cyan]Destino:[/cyan]\n
+    • Domínio: {dominio} \n
+    [bold green]Avaliação:[/bold green]\n
     • Pontuação de risco: {pontuacao} 
     • Nível de risco: {risco}
     • Motivos: {motivos}
-                    """,
-                   title="Detalhes da conexão de rede", width=85)
+    """,
+
+    title="Detalhes da conexão de rede", width=85)
     print(painel)
