9169fd27c2f4aa2b0220a9868f4df172be47fa69
George <georgehebo18@gmail.com>
2026-06-07 21:29:07 +0100

    Refactor: Implementação de blocos try / except , para prevenir crash geral do código e registar mensagens de erro na tabela de logs

--- modo_manual/analise_processo.py
+++ modo_manual/analise_processo.py
@@ -1,6 +1,7 @@
 import os
 import psutil
 from CLI import painel
+from CLI import cores
 from modulos import processos as p
 from modulos import logs as l
 from modulos import interface
@@ -34,13 +35,20 @@ def analisar_processo(tipos_assinatura):
     print("\n")
 
     for process in psutil.process_iter(['pid', 'ppid', 'name', 'username', 'exe']):
-        temporario['pid'] = process.pid
-        temporario['ppid'] = process.ppid()
-        temporario['nome'] = process.name()
-        temporario['utilizador'] = process.username()
+        try:
+            temporario['pid'] = process.pid
+            temporario['ppid'] = process.ppid()
+            temporario['nome'] = process.name()
+            temporario['utilizador'] = process.username()
 
-        if (temporario['nome'].endswith(".exe")):
-            processos.append(temporario.copy())
+            if (temporario['nome'].endswith(".exe")):
+                processos.append(temporario.copy())
+
+        except Exception as e:
+            print(f"{cores.CORES['vermelho']}Ocorreu um erro durante a análise de um processo (verificar logs de erro){cores.CORES['limpo']}")
+            erro = f"{type(e).__name__}: {e}"
+            l.inserir_log_erro("erro", "processos", erro)
+            continue
 
     item = selecionar_valor.selecionar_valor(processos, len(frase))
 
@@ -48,59 +56,75 @@ def analisar_processo(tipos_assinatura):
         return
 
     os.system("cls")
+
     try:
         P = psutil.Process(item['pid'])
         caminho = P.exe()
     except (psutil.AccessDenied, psutil.NoSuchProcess):
         caminho = "Acesso negado ou processo terminado"
 
-    resultado_consulta = p.verificar_dados_caminho(caminho, tipos_assinatura)
+    try:
+        resultado_consulta = p.verificar_dados_caminho(caminho, tipos_assinatura)
 
-    caminho = resultado_consulta['caminho']
-    status = resultado_consulta['status']
-    assinatura = resultado_consulta['assinatura_digital']
-    hash_processo = resultado_consulta['hash']
+        caminho = resultado_consulta['caminho']
+        status = resultado_consulta['status']
+        assinatura = resultado_consulta['assinatura_digital']
+        hash_processo = resultado_consulta['hash']
 
-    info_score = calcular_score_processo(ficheiro, item, status, caminho)
+        info_score = calcular_score_processo(ficheiro, item, status, caminho)
 
-    pontuacao = info_score[0]['pontuacao']
-    risco = info_score[0]['risco']
-    motivos = criar_string.criar_string_motivo(info_score[1])
-    painel.painel_de_processo(item['pid'], item['ppid'], item['nome'], caminho, item['utilizador'],
-                              hash_processo, assinatura, pontuacao, risco, motivos)
+        pontuacao = info_score[0]['pontuacao']
+        risco = info_score[0]['risco']
+        motivos = criar_string.criar_string_motivo(info_score[1])
+        painel.painel_de_processo(item['pid'], item['ppid'], item['nome'], caminho, item['utilizador'],
+                                  hash_processo, assinatura, pontuacao, risco, motivos)
 
-    if (l.consultar_processo(item['pid'])):
-        l.update_processo(item['pid'], item['utilizador'], pontuacao, risco, motivos)
-    else:
-        id_binario = l.consultar_binario(caminho)
-        l.inserir_processo(item['pid'], item['ppid'], item['nome'], item['utilizador'], pontuacao,
-                              risco, motivos, id_binario["id"])
+        if (l.consultar_processo(item['pid'])):
+            l.update_processo(item['pid'], item['utilizador'], pontuacao, risco, motivos)
+        else:
+            id_binario = l.consultar_binario(caminho)
+            l.inserir_processo(item['pid'], item['ppid'], item['nome'], item['utilizador'], pontuacao,
+                               risco, motivos, id_binario["id"])
 
-    resposta = validar_resposta.validar_resposta("Deseja terminar o processo")
+        resposta = validar_resposta.validar_resposta("Deseja terminar o processo")
 
-    if (resposta in ["SIM", "S"]):
-        processo.terminar_processo(item['pid'], caminho)
+        if (resposta in ["SIM", "S"]):
+            processo.terminar_processo(item['pid'], caminho)
+
+    except Exception as e:
+        print(f"{cores.CORES['vermelho']}Ocorreu um erro durante a consulta da tabela binários (verificar logs de erro){cores.CORES['limpo']}")
+        erro = f"{type(e).__name__}: {e}"
+        l.inserir_log_erro("erro", "processos", erro)
+
 
 def calcular_score_processo(ficheiro, processo, status, caminho):
     dados_score = {'pontuacao': 0, 'risco': ''}  # armazena todos os processos.txt considerados suspeitos.
     motivos = []
 
-    caminho_processo = normalizar_caminho.normalizar(caminho)
+    try:
+        caminho_processo = normalizar_caminho.normalizar(caminho)
 
-    score, motivo = pontos_assinatura.pontos_assinatura(status)
-    dados_score['pontuacao'] += score
+        score, motivo = pontos_assinatura.pontos_assinatura(status)
+        dados_score['pontuacao'] += score
 
-    if (status not in ["Valid", "Sistema"]):
-        motivos.append(motivo)
+        if (status not in ["Valid", "Sistema"]):
+            motivos.append(motivo)
 
-    if (caminho_raiz.verificar_caminho_raiz(caminho_processo)):
-        dados_score['pontuacao'] += 25
-        motivos.append("Programa na raiz do disco")
+        if (caminho_raiz.verificar_caminho_raiz(caminho_processo)):
+            dados_score['pontuacao'] += 25
+            motivos.append("Programa na raiz do disco")
 
-    score_local, motivos_locais = calcular_score.calcular_score_auxiliar(ficheiro, processo['nome'], caminho_processo)
+        score_local, motivos_locais = calcular_score.calcular_score_auxiliar(ficheiro, processo['nome'],
+                                                                             caminho_processo)
+        dados_score['pontuacao'] += score_local['pontuacao']
+        motivos.extend(motivos_locais)
 
-    dados_score['pontuacao'] += score_local['pontuacao']
-    motivos.extend(motivos_locais)
+    except Exception as e:
+        print(f"{cores.CORES['vermelho']}Ocorreu um erro durante o cálculo de score (verificar logs de erro){cores.CORES['limpo']}")
+        l.inserir_log_erro("erro","processos",f"{type(e).__name__}: {e}")
+
+        dados_score['pontuacao'] = 0
+        motivos = ["Erro no cálculo de score"]
 
     dados_score['pontuacao'] = max(0, min(dados_score['pontuacao'], 100))
     dados_score['risco'] = atribuir_risco.definir_risco(dados_score)
--- modulos/processos.py
+++ modulos/processos.py
@@ -1,5 +1,7 @@
 import os
 import psutil
+import traceback
+from CLI import cores
 from modulos import logs
 from uteis import verificar_assinatura_digital
 from uteis import obter_hash
@@ -23,25 +25,38 @@ def verificar_dados_caminho(caminho, tipos_assinatura):
         'status': ''
     }
 
-    resultado = logs.consultar_binario(caminho)
-    if resultado:
-        return resultado
+    try:
+        resultado = logs.consultar_binario(caminho)
+        if resultado:
+            return resultado
 
-    if caminho in ['Acesso negado ou processo terminado', '', 'Registry']:
-        dados['assinatura_digital'] = 'Ignorado (Sistema)'
-        dados['hash'] = 'Ignorado (Sistema)'
-        dados['status'] = 'Sistema'
+        if caminho in ['Acesso negado ou processo terminado', '', 'Registry']:
+            dados['assinatura_digital'] = 'Ignorado (Sistema)'
+            dados['hash'] = 'Ignorado (Sistema)'
+            dados['status'] = 'Sistema'
 
-    else:
-        assinatura_binario = verificar_assinatura_digital.verificar_assinatura(caminho)
+        else:
+            assinatura_binario = verificar_assinatura_digital.verificar_assinatura(caminho)
 
-        dados['hash'] = obter_hash.obter_hash(caminho)
-        dados['status'] = assinatura_binario
-        dados['assinatura_digital'] = tipos_assinatura.get(assinatura_binario,"Assinatura desconhecida")
+            dados['hash'] = obter_hash.obter_hash(caminho)
+            dados['status'] = assinatura_binario
+            dados['assinatura_digital'] = tipos_assinatura.get(assinatura_binario, "Assinatura desconhecida")
 
-    logs.inserir_binario(dados['caminho'],dados['hash'],dados['assinatura_digital'],dados['status'])
+        logs.inserir_binario(dados['caminho'], dados['hash'], dados['assinatura_digital'], dados['status'])
 
-    return dados
+        return dados
+
+    except Exception as e:
+        print(f"{cores.CORES['vermelho']}Ocorreu um erro ao consultar os dados do binário (verificar logs de erro){cores.CORES['limpo']}")
+        erro = f"{type(e).__name__}: {e}"
+        logs.inserir_log_erro("erro", "processos", erro)
+
+        return {
+            'caminho': caminho,
+            'hash': "Erro",
+            'assinatura_digital': "Erro na análise",
+            'status': "Erro"
+        }
 
 # =========================
 # FUNÇÕES DE PRINCIPAIS.
@@ -51,80 +66,101 @@ def obter_processos():
     Método obter_processos, serve para obter todos os processoas.
     :return: Devolve uma lista de todos os processos.txt (programas em execução)
     """
-    os.system("cls")
-    print("Processos Analisados: \n")
+    try:
+        os.system("cls")
+        print("Processos Analisados: \n")
 
-    processos = list()
-    temp = dict()
-    tipos_assinatura = {'Valid':'Válida', 'NotSigned':'Sem assinatura',
-                        'HashMismatch':'Ficheiro alterado', 'NotTrusted':'Certificado inválido',
-                        'UnknownError':'Erro na verificação da assinatura digital'}
+        processos = list()
+        temp = dict()
+        tipos_assinatura = {'Valid': 'Válida', 'NotSigned': 'Sem assinatura',
+                            'HashMismatch': 'Ficheiro alterado', 'NotTrusted': 'Certificado inválido',
+                            'UnknownError': 'Erro na verificação da assinatura digital'}
 
-    lista = carregar_lista.carregar_lista("listas/blacklist.txt")
+        lista = carregar_lista.carregar_lista("listas/blacklist.txt")
 
-    for process in psutil.process_iter(['pid', 'ppid', 'name', 'username', 'exe']):
-        # Tenta atribuir o caminho do executável.
-        try:
-            caminho = process.exe()
-        # Em caso de exceções atribui uma string a variavel.
-        except (psutil.AccessDenied, psutil.NoSuchProcess):
-            caminho = "Acesso negado ou processo terminado"
+        for process in psutil.process_iter(['pid', 'ppid', 'name', 'username', 'exe']):
+            try:
+                # Tenta atribuir o caminho do executável.
+                try:
+                    caminho = process.exe()
+                # Em caso de exceções atribui uma string a variavel.
+                except (psutil.AccessDenied, psutil.NoSuchProcess):
+                    caminho = "Acesso negado ou processo terminado"
 
-        resultado_consulta = verificar_dados_caminho(caminho, tipos_assinatura)
-        # Atribui o valor a  cada chave do dicionário
-        temp['pid'] = process.pid  # pid (process id)
-        temp['ppid'] = process.ppid()  # ppid (parent process id)
-        temp['nome'] = process.name()
-        temp['caminho'] = resultado_consulta['caminho']
-        temp['utilizador'] = process.username()
-        temp['hash'] = resultado_consulta['hash']
-        temp['assinatura'] = resultado_consulta['assinatura_digital']
-        temp['status'] = resultado_consulta['status']
-        temp['pontuacao'] = 0
-        temp['risco'] = ''
+                resultado_consulta = verificar_dados_caminho(caminho, tipos_assinatura)
+                # Atribui o valor a  cada chave do dicionário
+                temp['pid'] = process.pid  # pid (process id)
+                temp['ppid'] = process.ppid()  # ppid (parent process id)
+                temp['nome'] = process.name()
+                temp['caminho'] = resultado_consulta['caminho']
+                temp['utilizador'] = process.username()
+                temp['hash'] = resultado_consulta['hash']
+                temp['assinatura'] = resultado_consulta['assinatura_digital']
+                temp['status'] = resultado_consulta['status']
+                temp['pontuacao'] = 0
+                temp['risco'] = ''
 
-        item = calcular_score_processos(lista, temp.copy())
-        temp['pontuacao'] = item[0]['pontuacao']
-        temp['risco'] = item[0]['risco']
+                item = calcular_score_processos(lista, temp.copy())
+                temp['pontuacao'] = item[0]['pontuacao']
+                temp['risco'] = item[0]['risco']
 
-        motivo = criar_string.criar_string_motivo(item[1])
+                motivo = criar_string.criar_string_motivo(item[1])
 
-        processos_copia = temp.copy()
-        processos.append(processos_copia)
-        mostrar_processos([processos_copia], item[1])
+                processos_copia = temp.copy()
+                processos.append(processos_copia)
+                mostrar_processos([processos_copia], item[1])
 
-        if (logs.consultar_processo(temp['pid'])):
-            logs.update_processo(temp['pid'], temp['utilizador'], temp['pontuacao'], temp['risco'], motivo)
-        else:
-            id_binario = logs.consultar_binario(temp['caminho'])
-            logs.inserir_processo(temp['pid'], temp['ppid'], temp['nome'], temp['utilizador'], temp['pontuacao'], temp['risco'], motivo, id_binario["id"])
+                if (logs.consultar_processo(temp['pid'])):
+                    logs.update_processo(temp['pid'], temp['utilizador'], temp['pontuacao'], temp['risco'], motivo)
+                else:
+                    id_binario = logs.consultar_binario(temp['caminho'])
+                    logs.inserir_processo(temp['pid'], temp['ppid'], temp['nome'], temp['utilizador'],
+                                          temp['pontuacao'],
+                                          temp['risco'], motivo, id_binario["id"])
+            except Exception as e:
+                print(f"{cores.CORES['vermelho']}Ocorreu um erro durante a análise de um processo (verificar logs de erro){cores.CORES['limpo']}")
+                erro =  f"{type(e).__name__}: {e}"
+                logs.inserir_log_erro("erro", "processos", erro)
+                continue
+
+    except Exception as e:
+        print(f"{cores.CORES['vermelho']}Ocorreu um erro durante a análise (verificar logs de erro){cores.CORES['limpo']}")
+        erro = f"{type(e).__name__}: {e}"
+        logs.inserir_log_erro("erro", "processos", erro)
+        return
+
 
 def calcular_score_processos(ficheiro, processo):
-    """
-    Método obter_processos_suspeitos, serve para marcar um processo como suspeito com base numa blacklist (ficheiro de texto).
-    :param ficheiro: Corresponde ao ficheiro de texto usado como blacklist
-    :param lista_processos: Corresponde a lista de todos os processos.txt (retorno da função anterior)
-    :return: Uma lista com todos os processoas considerados suspeitos ou uma lista vazia (em caso de erro).
-    """
-    dados_score = {'pontuacao': 0, 'risco': ''}  # armazena todos os processos.txt considerados suspeitos.
+
+    dados_score = {'pontuacao': 0, 'risco': ''}
     motivos = []
 
-    caminho_processo = normalizar_caminho.normalizar(processo['caminho'])
+    try:
+        caminho_processo = normalizar_caminho.normalizar(processo['caminho'])
 
-    score, motivo = pontos_assinatura.pontos_assinatura(processo['status'])
-    dados_score['pontuacao'] += score
+        score, motivo = pontos_assinatura.pontos_assinatura(processo['status'])
+        dados_score['pontuacao'] += score
 
-    if (processo['status'] not in ["Valid", "Sistema"]):
-        motivos.append(motivo)
+        if processo['status'] not in ["Valid", "Sistema"]:
+            motivos.append(motivo)
 
-    if (caminho_raiz.verificar_caminho_raiz(caminho_processo)):
-        dados_score['pontuacao'] += 25
-        motivos.append("Programa na raiz do disco")
+        if caminho_raiz.verificar_caminho_raiz(caminho_processo):
+            dados_score['pontuacao'] += 25
+            motivos.append("Programa na raiz do disco")
 
-    score_local, motivos_locais = calcular_score.calcular_score_auxiliar(ficheiro, processo['nome'], caminho_processo)
+        score_local, motivos_locais = calcular_score.calcular_score_auxiliar(
+            ficheiro,
+            processo['nome'],
+            caminho_processo)
 
-    dados_score['pontuacao'] += score_local['pontuacao']
-    motivos.extend(motivos_locais)
+        dados_score['pontuacao'] += score_local['pontuacao']
+        motivos.extend(motivos_locais)
+
+    except Exception as e:
+        print(f"{cores.CORES['vermelho']}Ocorreu um erro durante o cálculo de score (verificar logs de erro){cores.CORES['limpo']}")
+        logs.inserir_log_erro("erro","processos",f"{type(e).__name__}: {e}")
+        dados_score['pontuacao'] = 0
+        motivos = ["Erro no cálculo de score"]
 
     dados_score['pontuacao'] = max(0, min(dados_score['pontuacao'], 100))
     dados_score['risco'] = atribuir_risco.definir_risco(dados_score)
