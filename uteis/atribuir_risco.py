ecede9ad2785a555fab4dadb15d28af20985c097
George <georgehebo18@gmail.com>
2026-06-09 11:00:24 +0100

    Refactor: Remoção de utilização de cores através do módulo CLI

--- uteis/atribuir_risco.py
+++ uteis/atribuir_risco.py
@@ -1,10 +1,9 @@
-from CLI import cores
 def definir_risco(dados_score):
     risco = ''
     if (dados_score['pontuacao'] >= 0 and dados_score['pontuacao'] <= 30):
-        risco = f'{cores.CORES['verde']}Baixo{cores.CORES['limpo']}'
+        risco = 'Baixo'
     elif (dados_score['pontuacao'] > 30 and dados_score['pontuacao'] <= 60):
-        risco = f'{cores.CORES['amarelo']}Médio{cores.CORES['limpo']}'
+        risco = 'Médio'
     else:
-        risco = f'{cores.CORES['vermelho']}Alto{cores.CORES['limpo']}'
+        risco = 'Alto'
     return risco
