17c35f9553f6f45f7219e993f772261b5ed58eeb
George <georgehebo18@gmail.com>
2026-06-09 01:57:08 +0100

    Refactor: Estilização do output, utilizando cores para indicar o nível de severidade de cada item, com base na pontuação de risco

--- uteis/atribuir_risco.py
+++ uteis/atribuir_risco.py
@@ -1,9 +1,10 @@
+from CLI import cores
 def definir_risco(dados_score):
     risco = ''
     if (dados_score['pontuacao'] >= 0 and dados_score['pontuacao'] <= 30):
-        risco = 'Baixo'
+        risco = f'{cores.CORES['verde']}Baixo{cores.CORES['limpo']}'
     elif (dados_score['pontuacao'] > 30 and dados_score['pontuacao'] <= 60):
-        risco = 'Médio'
+        risco = f'{cores.CORES['amarelo']}Médio{cores.CORES['limpo']}'
     else:
-        risco = 'Alto'
+        risco = f'{cores.CORES['vermelho']}Alto{cores.CORES['limpo']}'
     return risco
